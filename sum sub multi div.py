ca40b64a0121c314360c517abbd334ea106777c3
priyanshuborad007-creator <priyanshuborad007@gmail.com>
2026-01-06 16:00:24 +0530

    Add files via upload

---
+++ sum sub multi div.py
@@ -0,0 +1,48 @@
+a=40
+b=60
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+a=int(input("enter the hour:"))
+b=a*60
+print("minutes:",b)
+a=int(input("enter the minutes:"))
+b=a/60
+print("hour:",b)
+a=int(input("enter the dollar:"))
+b=a*48
+print("rupee",b)
+a=int(input("enter the dollar:"))
+b=a*48/70
+print("the pound is ",b)
+a=int(input("enter the gram:"))
+b=a/1000
+print("the kilogram is:",b)
+a=int(input("enter the kilogram:"))
+b=a*1000
+print("the gram is:",b)
+a=int(input("enter the bytes:"))
+b=a/1024
+print("the KB is:",b)
+a=int(input("enter the bytes:"))
+b=a/1024*1024
+print("the MB is:",b)
+a=int(input("enter the bytes:"))
+b=a/1024*1024*1024
+print("the GB is:",b)
+a=int(input("enter the celcius:"))
+b=(9/5*a) +32
+print("the farhenit is:",b)
+
+
+
+
+
+
+
+
+
+
+
+
